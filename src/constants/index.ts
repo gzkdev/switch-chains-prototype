7848bb643b01e6c734e11f2d2a2127370bf31999
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-03-26 18:57:54 +0100

    update app config

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const ALCHEMY_API_KEY = import.meta.env.ALCHEMY_API_KEY;
+export const INFURA_API_KEY = import.meta.env.INFURA_API_KEY;
+export const WALLETCONNECT_PROJECT_ID = import.meta.env.WALLETCONNECT_PROJECT_ID;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly WALLETCONNECT_PROJECT_ID: string;
+  readonly ALCHEMY_API_KEY: string;
+  readonly INFURA_API_KEY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
